6c261ae73786b32996446226e3d90f8e3523ce01
hugo <hugo-mehdi.taj@epitech.eu>
2024-03-15 12:24:32 +0100

    [add] main and interface

---
+++ main.py
@@ -0,0 +1,8 @@
+from interface import MyTk
+
+if __name__ == '__main__':
+    print("Lancement du jeu...")
+    # Création de l'instance de l'application et lancement de la boucle principale
+    app = MyTk()
+    app.mainloop()
+    print("Fermeture du jeu...")
